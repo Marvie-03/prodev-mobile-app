b307864c52d9ee61412d36c2f5b1cac068d2a9ad
Marvie-03 <kayveengee@gmail.com>
2025-11-08 14:11:22 -0800

    Layout Update

--- prodev-mobile-app-0x06/app/(home)/layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/layout.tsx
@@ -5,6 +5,7 @@ const HomeRootLayout = () => {
   return (
     <Tabs screenOptions={{
         tabBarActiveTintColor: '#34967C',
+        headerShown: false
     }} >
       <Tabs.Screen name="index" options={{
         title: 'Home',
@@ -27,6 +28,7 @@ const HomeRootLayout = () => {
       }} />
         <Tabs.Screen name="profile" options={{
         title: 'Profile',
+        headerShown: false,
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
       }} />
     </Tabs>
